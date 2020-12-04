ba93dd8281ead3868fce9c5dbcd120e3ee5d05aa
Ivy Trainor <tytrainor@hotmail.com>
2020-12-04 06:53:03 +0100

    added a js folder to add js files, toggle menu almost done and navbar done

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+ function onClickMenu() {
+     document.getElementById("menu-lines").classList.toggle("change");
+     /* getting specific  menu by it´s id */
+     /**toggle changes the class everytime the user uses/clicks toggle-menu */
+
+     /* function for adding navbar items to toggle */
+    document.getElementById("nav-bar").classList.toggle("change");
+ }
+
