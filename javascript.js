1462b73d16da568075615c044f2be1c1e7c999e8
Avilin <avilin.persson@gmail.com>
2020-12-04 09:07:42 +0100

    admin page

--- javascript.js
+++ javascript.js
@@ -14,3 +14,37 @@ function myFunction() {
       moreText.style.display = "inline";
     }
   }
+
+
+var newProductList = []; // Global array for new products   
+
+
+// Creating a function to add new products in admin.html
+
+function addPrd() {
+
+  // var prdImage
+  var headline = document.querySelector("#headline").value; // Headline
+  var prdName = document.querySelector("#prd-desp").value; // Product Description
+  var prdPrice = document.querySelector("#price").value*1; // Product Price
+
+  var li = document.createElement("li"); // declares and creates li element
+  li.innerText = headline + " " + prdName + " " + prdPrice; // chooses what data to put into li element 
+  var ul = document.querySelector(".admin-prd-list") // declares and choose pre-existing element from html  
+  ul.appendChild(li); // appends JS-created li element to ul element in html
+
+  newProductList.push
+
+}
+
+
+// Publish new products onto webshop/index.html
+
+function publishPrd() {
+ 
+
+
+
+  
+
+}
