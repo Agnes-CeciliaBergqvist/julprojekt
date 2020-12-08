9b531d0a9fde3f84d0b25d899633d36a8ac38a14
Ivy Trainor <tytrainor@hotmail.com>
2020-12-08 11:19:40 +0100

    converting back to previous version of mainm

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-//Read more button
-function myFunction() {
+//Read more button - product card
+/*function myFunction() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
@@ -13,4 +13,80 @@ function myFunction() {
       btnText.innerHTML = "Read less";
       moreText.style.display = "inline";
     }
-  }
+  }*/
+
+  //Load more products button
+
+  /*function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }*/
+
+
+
+
+
+
+
+// var newProductList = []; // Global array for new products   
+
+// A function to add new products in admin.html
+function addPrd() {
+
+  var headline = document.querySelector("#headline").value; // Headline
+  var prdName = document.querySelector("#prd-desp").value; // Product Description
+  var prdPrice = document.querySelector("#price").value*1; // Product Price
+  
+  // declares and appends set image to news products list on admin.html
+  var prdImage = document.createElement("img"); // declares and creates img element
+
+
+  // VILL UPPDATERA IMG SRC NEDAN TILL SRC FRÅN UPPLADDAT FORMULÄR, MEN HUR?
+  prdImage.src = "images/opi04.webp"; // sets image source
+
+
+  document.querySelector(".admin-prd-list").appendChild(prdImage);
+  console.log(prdImage)
+
+
+
+  // appends data list (new products) on admin.html 
+  var li = document.createElement("li"); // declares and creates li element
+  li.innerText = headline + " " + prdName + " " + prdPrice; // chooses what data to put into li element 
+  console.log(li)
+  
+  var ul = document.querySelector(".admin-prd-list"); // declares and choose pre-existing element from html  
+  ul.appendChild(li); // appends JS-created li element to ul element in html
+
+ 
+
+}
+
+
+
+
+
+
+/*
+// local storage js, loopa igenom - Rakib kommer gå igenom 
+
+// Publish new products onto webshop/index.html 
+function publishPrd() {
+
+  // var prdImage
+  var headline = document.querySelector("#headline").value; // Headline
+  var prdName = document.querySelector("#prd-desp").value; // Product Description
+  var prdPrice = document.querySelector("#price").value * 1; // Product Price
+
+  // appends data list (new products) on index.html 
+  var li = document.createElement("li");
+  li.innerText = headline + " " + prdName + " " + prdPrice;
+  var ul = document.querySelector(".index-prd-list");
+  ul.appendChild(li); 
+
+}
+*/
--- js/navbar.js
+++
@@ -1,9 +0,0 @@
- function onClickMenu() {
-     document.getElementById("menu-lines").classList.toggle("change");
-     /* getting specific  menu by it´s id */
-     /**toggle changes the class everytime the user uses/clicks toggle-menu */
-
-     /* function for adding navbar items to toggle */
-    document.getElementById("nav-bar").classList.toggle("change");
- }
-
