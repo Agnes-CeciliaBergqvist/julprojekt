0f1b8ac305591c78463d024ccee680dc3762c4f8
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2020-12-08 17:39:50 +0100

    started off with JS code for the shopping cart when you click on 'add to cart'

--- javascript.js
+++ javascript.js
@@ -90,3 +90,19 @@ function publishPrd() {
 
 }
 */
+
+//function for adding product to shopping cart 
+var addToCartButtons = document.getElementsByClassName('index-btn-flex')
+for (var i = 0; i < addToCartButtons.length; i++){
+  var button = addToCartButtons[i]
+  button.addEventListener('click', addToCartClicked)
+
+}
+//function for clicking on the button 
+function addToCartClicked(event) {
+  var button = event.target
+  var card = button.parentElement.parentElement //id name and parent to product card
+  var h3 = card.getElementByIdName('index-h3')[0].innerText //Title and the first product
+  console.log(h3)
+  
+}
