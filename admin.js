8b1f8d48e948763d463031bb5f1ce6600ad0e983
Avilin <avilin.persson@gmail.com>
2020-12-14 13:58:50 +0100

    updates for admin page

--- admin.js
+++ admin.js
@@ -1,41 +1,47 @@
- // ADMIN PAGE - CREATE, EDIT AND SOON DELETE PRODUCTS 
+// ADMIN PAGE - CREATE, EDIT AND DELETE PRODUCTS 
 
 // global variable storing the list of names
 // this can be used for local storage and to be used for published new prds
-var names = [];
+var adminNewPrdsList = [];
 
 // keep track of which item is selected
 var selected_name_wrapper = null;
 var selected_name_index = null;
 
-function addName() {
+function addProduct() {
     // values from input fields 
-    var name = document.getElementById("name-input").value;
-    var name2 = document.getElementById("decsrip-input").value;
-    var price = document.getElementById("price-input").value;
 
-    // values pushed into list var names (mutable)
-    if(name + name2 + price) {
-        names.push(name + " " + name2 + " " + price);
+
+    var adminHeadline = document.querySelector("#admin-headline-input").value;
+    var adminDescription = document.querySelector("#admin-decsrip-input").value;
+    var adminPrice = document.querySelector("#admin-price-input").value;
+
+    // values pushed into list var adminNewPrdsList (mutable)
+    if (adminHeadline + adminDescription + adminPrice) {
+        adminNewPrdsList.push(adminHeadline + " " + adminDescription + " " + adminPrice);
         updateHTML();
     }
+    // fixa till objekt istället likt i local storage
+    // lagra i local storage 
 
-    console.log(names)
+    console.log(adminNewPrdsList)
 }
+
+
 
 // updates the html-file
 function updateHTML() {
-    var list = document.getElementById("list");
+    var list = document.getElementById("admin-list");
     list.innerHTML = "";
-  
-    for(let i=0; i<names.length; i++) {
+
+    for (let i = 0; i < adminNewPrdsList.length; i++) {
         var wrapper = document.createElement("div")
-        wrapper.setAttribute("id", names[i]) // id + dynamic prd array
+        wrapper.setAttribute("id", adminNewPrdsList[i]) // id + dynamic prd array
         wrapper.addEventListener("click", updateSelectedName)
         console.log(wrapper)
 
         var div_name = document.createElement("div");
-        div_name.appendChild(document.createTextNode(names[i]))
+        div_name.appendChild(document.createTextNode(adminNewPrdsList[i]))
 
         wrapper.appendChild(div_name)
         list.appendChild(wrapper)
@@ -47,7 +53,7 @@ function updateHTML() {
 function updateSelectedName() {
     selected_name_wrapper = this; // global var
     var selected_name = this.getAttribute("id") // fetches id + dynamic prd array
-    selected_name_index = names.indexOf(selected_name) // global var
+    selected_name_index = adminNewPrdsList.indexOf(selected_name) // global var
 
 
     var p = document.createElement("p")
@@ -56,36 +62,36 @@ function updateSelectedName() {
     chosenName.appendChild(p);
 
     console.log("selecting: " + selected_name)
-} 
+}
 
 
 // When clicking on edit after above step,
 // fills the input box with the name that you want to edit
 function editName() {
-    if(selected_name_wrapper) {
+    if (selected_name_wrapper) {
         var name = selected_name_wrapper.getAttribute("id")
-        document.getElementById("prd-to-edit").value = name;
+        document.getElementById("admin-prd-to-edit").value = name;
         console.log("start editing name")
     }
 }
 
 function updateName() {
-    var new_name = document.getElementById("prd-to-edit").value
-    if(selected_name_wrapper){
-        names[selected_name_index] = new_name;
-      
+    var new_name = document.getElementById("admin-prd-to-edit").value
+    if (selected_name_wrapper) {
+        adminNewPrdsList[selected_name_index] = new_name;
+
         selected_name_wrapper = null;
         selected_name_index = null;
-      
+
         updateHTML();
 
-        // appends list _if_ it's updated, otherwise it's only below "List of Names" 
+        // appends list _if_ it's updated, otherwise it's only below "List of adminNewPrdsList" 
         // var p = document.createElement("p")
         // p.innerText = new_name;
         // var updatedName = document.querySelector("#admin-updated-list");
         // updatedName.appendChild(p);
 
-        
+
         // console log updated list above publish btn
         console.log("Ready to publish " + new_name)
         var pUpdateText = document.createElement("p")
@@ -97,28 +103,77 @@ function updateName() {
 }
 
 
-// AP : function to remove items from var names.push()
+// function to remove items from var adminNewPrdsList.push()
 function deleteItems() {
-    var removeList = document.querySelector("div");   // chooses element to delete
+
+    var removeList = document.querySelector("#admin-list");   // chooses element to delete
     removeList.removeChild(removeList.lastChild);     //removes last item in array 
 
-//    names.removeChild(selected_name_wrapper);
+    // remove selected item in list; not functioning
+    //adminNewPrdsList.removeChild(selected_name_wrapper);
 
 }
 
 
 document.getElementById("admin-delete-btn").addEventListener("click", deleteItems)
-document.getElementById("add-name").addEventListener("click", addName)
-document.getElementById("edit").addEventListener("click", editName)
-document.getElementById("update").addEventListener("click", updateName)
+document.getElementById("admin-add-name").addEventListener("click", addProduct)
+document.getElementById("admin-edit").addEventListener("click", editName)
+document.getElementById("admin-update").addEventListener("click", updateName)
 
 
 
 
 
-// use local storage and global array var names to push to index.html
-// function with local storage?
 
-// call function with publish button in html
-//document.getElementById("admin-publish").addEventListener("click", xxx)
+function addToLocalStorage() {
+    let product = {}
+    const name = document.querySelector("#input1").value;
+    const price = document.querySelector("#input2").value;
+
+    // const divData = document.querySelector(".test").innerText;
+
+    // localStorage.setItem("DivData" , divData)
+
+    // input1 och input2 ska lagras i ett objekt
+    //Validera om användare har matat in empty value
+    product.name = name;
+    product.price = price;
+    //console.log(product) // {name :" ", price:" "}
+    //objektet ska pushas i item array 
+
+    item.push(product);
+    // console.log("item array ", item)
+    //item array ska lagras i localstorage 
+
+    // vi behöver skriva lite logik som gör att setItem kan också
+    //lagra nya value /append nya value
+    //när det finns data in i localstorage ska vi lägga till data  
+
+
+    // vi skulle spara data i localStorage 
+
+
+
+    // om det redan finns produkter i localstorage skulle vi lägga till item 
+    // i localstorage.
+
+
+    //localstorage har befietligt value
+    const localData = localStorage.getItem("productList");
+    //konverterade vi till js objekt 
+    const existingData = JSON.parse(localData);
+
+    // hur vi kan adda två array 
+    // const cleanedData = existingData +item   eller   item
+
+    const cleanedData = existingData ? existingData.concat(item) : item;
+
+    //cleanedData ska sparas i localstorage
+    localStorage.setItem("productList", JSON.stringify(cleanedData));
+
+    console.log(localData)
+    console.log(addToLocalStorage)
+}
+
+
 
