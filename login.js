56b3ecd938bc720ad1f110e3e4370606c955a60f
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2020-12-28 14:03:55 +0100

    chnaged comments on login.html and login.js

--- login.js
+++ login.js
@@ -1,22 +1,27 @@
-var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
+// Code to be able to logn on login.html
+
+// Variable to count number of attempts to login
+var attempt = 3; 
+
+// Function executes when clicking on "login button"
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
+
 if ( username == "essie" && password == "nagellack123"){
 alert ("Login successfully");
-window.location = "admin.html"; // Redirecting to other page.
+window.location = "admin.html"; // Redirecting to other page
 return false;
 }
 else{
-attempt --;// Decrementing by one.
+attempt --;// Decrementing by one
 alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
+
+// Disabling fields after 3 attempts
 if( attempt == 0){
 document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
 document.getElementById("submit").disabled = true;
 return false;
 }
-}
-}
+}}
